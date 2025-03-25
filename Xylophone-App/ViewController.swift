97ee5709194c807b84467f53bd6780b0118cdf95
jscheth <sovichea.chet@gmail.com>
2025-03-25 13:41:12 -0700

    Add playSound function

--- Xylophone-App/ViewController.swift
+++ Xylophone-App/ViewController.swift
@@ -6,12 +6,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        if let title = sender.configuration?.title {
+            print("Button title: \(title)")
+        } else {
+            print("No title found!")
+        }
+//        playSound(soundTitle: sender.currentTitle!)
+//        sender.alpha = 0.5
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+//            sender.alpha = 1.0
+//        }
+    }
+    
+    
+    func playSound(soundTitle: String) {
+        let url = Bundle.main.url(forResource: soundTitle, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
     }
 
 
